1e6e19478b7268f3f289eccc495fba62f83e8e7c
Benoit Dufumier <benoit.dufumier@supelec.fr>
2018-10-04 12:50:23 -0400

    utils file uploaded. Paths should be changed

--- CNN/utils.py
+++ CNN/utils.py
@@ -1,74 +1,92 @@
 
             # CONFIG FOR SOLAR FLARES DATA
 config = { 'SF': {'data_dims': [None, None, 3], # MAX DIM on 7/20 : [2860, 2587] 
-                  'batch_memsize': 16384, # xMB / global batch
-                  'num_threads' : 48,
-                  'model': 'VGG_16',
-                  'checkpoint': '/nobackup/bdufumie/SolarFlaresProject/Checkpoints/SF',
-                  'tensorboard': '/nobackup/bdufumie/SolarFlaresProject/Tensorboard/SF',
-                  'input_features_dir' : '/nobackup/bdufumie/SolarFlaresProject/Dataset/SF',
-                  'output_features_dir': '/nobackup/bdufumie/SolarFlaresProject/Data/SF_LSTM/train',
+                  'batch_memsize': 1024 , # xMB / global batch
+                  'num_threads' : 8,
+                  'model': 'LRCN',
+                  'pb_kind': 'classification',
+                  'checkpoint': '/n/midland/w/dufumier/Documents/SolarFlaresProject/CNN/Checkpoints/SF',
+                  'tensorboard': '/n/midland/w/dufumier/Documents/SolarFlaresProject/CNN/Tensorboard/logs_SF',
+                  'input_features_dir' : '/n/midland/w/dufumier/Documents/SolarFlaresProject/CNN/Dataset/SF',
+                  'output_features_dir': '/n/midland/w/dufumier/Documents/SolarFlaresProject/CNN/Dataset/SF',
                   'learning_rate': 0.01,
                   'loss_weights': [1, 1.5],
                   'tolerance': 0.001,
                   'batch_norm': True,
-                  'dropout_prob': 0.3,
+                  'dropout_prob': 0.2,
                   'nb_classes': 2,
-                  'batch_size': 16, # nb pictures / local batch,
-                  'prefetch_buffer_size': 16, 
-                  'num_epochs': 10, # nb global batchs considered 
-                  'checkpoint_iter': 100, # save every 'checkpoint_iter' global iteration
+                  'batch_size': 1, # nb pictures / local batch,
+                  'prefetch_buffer_size': 1, 
+                  'num_epochs': 100, # nb total epochs
+                  'checkpoint_iter': 1000, # save every 'checkpoint_iter' global iteration
                   'ar_attrs' : ['T_REC', 'NOAA_AR', 'HARPNUM', 'LAT_FWT', 'LON_FWT',
                                 'SIZE', 'SIZE_ACR', 'NACR', 'NPIX', 'LAT_MIN', 'LAT_MAX',
                                 'LON_MIN', 'LON_MAX'],
                   'segs': ['Bp', 'Br', 'Bt'],
                   'goes_attrs' : ['event_class', 'noaa_active_region', 'event_date', 'start_time', 'end_time', 'peak_time'],
                   'subsampling' : 1,
-                  'resize_method': 'LIN_RESIZING',
+                  'resize_method': 'NONE',
+                  'rescaling_factor': 1,
+                  'display' : True,
                   'time_step': 60, # time step used in each video
-                  'training_paths': ['/nobackup/bdufumie/SolarFlaresProject/Data/SF/train/B-class-flares', 
-                                     '/nobackup/bdufumie/SolarFlaresProject/Data/SF/train/M-X-class-flares'],
-                  'testing_paths': ['/nobackup/bdufumie/SolarFlaresProject/Data/SF/test/B-class-flares', 
-                                     '/nobackup/bdufumie/SolarFlaresProject/Data/SF/test/M-X-class-flares'],              
+                  'training_paths': ['/n/midland/w/dufumier/Documents/SolarFlaresProject/DataQuery/SF-HDF5/train/B-class-flares',
+                                     '/n/midland/w/dufumier/Documents/SolarFlaresProject/DataQuery/SF-HDF5/train/M-X-class-flares'],
+                  'testing_paths': ['/n/midland/w/dufumier/Documents/SolarFlaresProject/DataQuery/SF-HDF5/test/B-class-flares',
+                                    '/n/midland/w/dufumier/Documents/SolarFlaresProject/DataQuery/SF-HDF5/test/M-X-class-flares']        
                   },
-            # CONFIG FOR SOLAR FLARE FEATURES EXTRACTED FROM THE CNN
-            'SF_LSTM': {'data_dims': [None,  1025], # MAX_T_STEP x N_INPUT
-                  'batch_memsize': 1024, # xMB / global batch
+            # CONFIG FOR SOLAR FLARE FEATURES EXTRACTED FROM THE ENCODER
+            'SF_encoded': {'data_dims': [None, None, None, 512],
+                  'batch_memsize': 1024 , # xMB / global batch
                   'num_threads' : 8,
-                  'model': 'LSTM',
-                  'checkpoint': '/nobackup/bdufumie/SolarFlaresProject/Checkpoints/SF_LSTM',
-                  'tensorboard': '/nobackup/bdufumie/SolarFlaresProject/Tensorboard/SF_LSTM',
+                  'model': 'LRCN',
+                  'pb_kind': 'classification',
+                  'checkpoint': '/n/midland/w/dufumier/Documents/SolarFlaresProject/CNN/Checkpoints/SF',
+                  'tensorboard': '/n/midland/w/dufumier/Documents/SolarFlaresProject/CNN/Tensorboard/logs_SF',
+                  'input_features_dir' : '/n/midland/w/dufumier/Documents/SolarFlaresProject/CNN/Dataset/SF',
+                  'output_features_dir': '/n/midland/w/dufumier/Documents/SolarFlaresProject/CNN/Dataset/SF',
                   'learning_rate': 0.01,
-                  'loss_weights': [1, 2],
+                  'loss_weights': [1, 1.5],
                   'tolerance': 0.001,
+                  'batch_norm': True,
+                  'dropout_prob': 0.2,
                   'nb_classes': 2,
-                  'batch_size': 40000, # nb sequences / local batch
-                  'prefetch_buffer_size': 40000, 
-                  'num_epochs': 15, # nb global batchs considered 
-                  'checkpoint_iter': 20000, # save every 'checkpoint_iter' global iteration                
-                  'time_step': 60, # time step used in each video
-                  'training_paths': ['/nobackup/bdufumie/SolarFlaresProject/Data/SF_LSTM/train'],
-                  'testing_paths': ['/nobackup/bdufumie/SolarFlaresProject/Data/SF_LSTM/test'],              
+                  'batch_size': 1, # nb videos / batch,
+                  'prefetch_buffer_size': 1, 
+                  'num_epochs': 100, # nb total epochs
+                  'checkpoint_iter': 1000, # save every 'checkpoint_iter' global iteration
+                  'ar_attrs' : [],
+                  'segs': ['filter_{}'.format(k) for k in range(512)],
+                  'subsampling' : 1,
+                  'resize_method': 'ZERO_PADDING',
+                  'rescaling_factor': 1,
+                  'display' : True,
+                  'time_step': 60, # time step used in each video (in minutes)
+                  'training_paths': ['/n/midland/w/dufumier/Documents/SolarFlaresProject/DataQuery/SF_encoded-HDF5/train'],
+                  'testing_paths': ['/n/midland/w/dufumier/Documents/SolarFlaresProject/DataQuery/SF_encoded-HDF5/test']                
                   },
            # CONFIG FOR MNIST DATA
-          'MNIST': {'data_dims': [28, 28, 1],
+          'MNIST': {'data_dims': [64, 64, 1],
                       'batch_memsize': 4*1024, # 4GB / batch
                       'num_threads' : 8,
-                      'model': 'VGG_16',
-                      'checkpoint': '/nobackup/bdufumie/SolarFlaresProject/Checkpoints/MNIST',
-                      'tensorboard': '/nobackup/bdufumie/SolarFlaresProject/Tensorboard/MNIST',
+                      'model': 'VGG_16_encoder_decoder',
+                      'pb_kind': 'encoder',
+                      'checkpoint': '/n/midland/w/dufumier/Documents/SolarFlaresProject/CNN/Checkpoints/MNIST',
+                      'tensorboard': '/n/midland/w/dufumier/Documents/SolarFlaresProject/CNN/Tensorboard/logs_MNIST',
                       'learning_rate': 0.01,
                       'loss_weights': 10*[1],
                       'tolerance': 0.01,
                       'batch_norm': True,
                       'dropout_prob': 0.3,
                       'nb_classes': 10,
-                      'batch_size': 256, # nb pictures / batch,
-                      'prefetch_buffer_size': 256, 
-                      'num_epochs': 1, # nb batchs considered 
+                      'batch_size': 128, # nb pictures / batch,
+                      'prefetch_buffer_size': 128,
+                      'resize_method': 'LIN_RESIZING',
+                      'segs': ['Nb'],
+                      'num_epochs': 10,
+                      'display' : True,
                       'checkpoint_iter': 500, # save every 500 global iteration
-                      'training_paths': [''],
-                      'testing_paths': [''],
+                      'training_paths': ['/n/midland/w/dufumier/Documents/SolarFlaresProject/DataQuery/MNIST-HDF5/train'],
+                      'testing_paths': ['/n/midland/w/dufumier/Documents/SolarFlaresProject/DataQuery/MNIST-HDF5/test'],
                       'subsampling':1
                       },   
           # CONFIG FOR CIFAR10
@@ -86,7 +104,10 @@ config = { 'SF': {'data_dims': [None, None, 3], # MAX DIM on 7/20 : [2860, 2587]
                       'nb_classes': 10,
                       'batch_size': 256, # nb pictures / batch,
                       'prefetch_buffer_size': 256,
+                      'resize_method': 'NONE',
+                      'segs': ['R', 'G', 'B'],
                       'num_epochs': 1, # nb batchs considered 
+                      'display' : True,
                       'checkpoint_iter': 500, # save every 500 global iteration
                       'training_paths': [''],
                       'testing_paths': [''],
